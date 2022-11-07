58b9851ec97f97603d2f8cbe76a8ae7aaa194897
xxeol2 <jhj01177@naver.com>
2022-11-07 11:39:14 +0900

    init: setup project

---
+++ src/main/java/kr/co/chingubang/ChingubangApplication.java
@@ -0,0 +1,13 @@
+package kr.co.chingubang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChingubangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChingubangApplication.class, args);
+	}
+
+}
