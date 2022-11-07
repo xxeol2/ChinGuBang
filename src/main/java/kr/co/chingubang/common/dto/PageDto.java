f6ffaf79db81c6b26f6be68d43fcab16c71ce810
xxeol2 <jhj01177@naver.com>
2022-11-07 12:52:18 +0900

    [INIT] 프로젝트 세팅 (#2)
    * init: setup project
    
    * chore: 기본 세팅

---
+++ src/main/java/kr/co/chingubang/common/dto/PageDto.java
@@ -0,0 +1,34 @@
+package kr.co.chingubang.common.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+
+import lombok.Getter;
+
+@Getter
+public class PageDto<T> implements Serializable {
+	private final List<T> data;
+	private final int page;
+	private final int size;
+	private final int totalPages;
+	private final long totalElements;
+	private final boolean firstPage;
+	private final boolean lastPage;
+
+	private PageDto(final Page<T> page) {
+		this.data = page.getContent();
+		this.page = page.getNumber();
+		this.size = page.getSize();
+		this.totalPages = page.getTotalPages();
+		this.totalElements = page.getTotalElements();
+		this.firstPage = page.isFirst();
+		this.lastPage = page.isLast();
+	}
+
+	public static <T> ResponseEntity<PageDto<T>> ok(final Page<T> page) {
+		return ResponseEntity.ok(new PageDto<>(page));
+	}
+}
---
+++ src/test/java/kr/co/chingubang/ChingubangApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.chingubang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChingubangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
