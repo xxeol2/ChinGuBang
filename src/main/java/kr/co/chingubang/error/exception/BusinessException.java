ccea359a07980d010df580855e819210111ef74c
xxeol2 <jhj01177@naver.com>
2022-11-07 17:14:23 +0900

    refactor: 에러 형식 변경

--- src/main/java/kr/co/chingubang/error/exception/BusinessException.java
+++ src/main/java/kr/co/chingubang/error/exception/BusinessException.java
@@ -1,31 +1,24 @@
 package kr.co.chingubang.error.exception;
 
-import org.springframework.http.HttpStatus;
-
 import kr.co.chingubang.error.dto.ErrorMessage;
 import lombok.Getter;
 
 @Getter
 public abstract class BusinessException extends RuntimeException {
-	private final HttpStatus status;
+	private final ErrorMessage errorMessage;
 
-	public BusinessException(String message, HttpStatus status) {
-		super(message);
-		this.status = status;
-	}
-
-	public BusinessException(Throwable cause, HttpStatus status) {
-		super(cause);
-		this.status = status;
-	}
-
-	public BusinessException(String message, Throwable cause, HttpStatus status) {
-		super(message, cause);
-		this.status = status;
-	}
-
-	public BusinessException(ErrorMessage message, HttpStatus status) {
+	public BusinessException(ErrorMessage message) {
 		super(message.getDescription());
-		this.status = status;
+		this.errorMessage = message;
+	}
+
+	public BusinessException(ErrorMessage message, String reason) {
+		super(reason);
+		this.errorMessage = message;
+	}
+
+	public BusinessException(String reason) {
+		super(reason);
+		this.errorMessage = ErrorMessage.CONFLICT_ERROR;
 	}
 }
