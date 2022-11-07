5620edac83c2573fb84757d9e3b66da63263a306
xxeol2 <jhj01177@naver.com>
2022-11-07 18:02:32 +0900

    refactor: 에러핸들링 수정

--- src/main/java/kr/co/chingubang/error/GlobalExceptionHandler.java
+++ src/main/java/kr/co/chingubang/error/GlobalExceptionHandler.java
@@ -1,5 +1,9 @@
 package kr.co.chingubang.error;
 
+import java.util.Arrays;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,16 +19,39 @@ public class GlobalExceptionHandler {
 
 	@ExceptionHandler(BusinessException.class)
 	protected ResponseEntity<ErrorDto> handleBusinessException(
-		final BusinessException e) {
-		log.error("[ERROR] Exception -> {}", e.getMessage());
-		return ResponseEntity.status(e.getStatus())
-			.body(new ErrorDto(e.getMessage(), e.getCause().toString()));
+		final BusinessException e,
+		final HttpServletRequest request
+	) {
+		log.error("BusinessException -> {} {}", e.getErrorMessage(), e.getCause());
+		log.error("Request url {}", request.getRequestURL());
+
+		return ResponseEntity
+			.status(e.getErrorMessage().getStatus())
+			.body(
+				new ErrorDto(
+					e.getErrorMessage().name(),
+					e.getErrorMessage().getDescription())
+			);
 	}
 
 	@ExceptionHandler(Exception.class)
-	protected ResponseEntity<ErrorDto> handleException(final Exception e) {
-		log.error("[ERROR] Exception -> {}", e.getMessage());
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-			.body(new ErrorDto(e.getMessage(), e.getCause().toString()));
+	protected ResponseEntity<ErrorDto> handleException(
+		final Exception e,
+		final HttpServletRequest request
+	) {
+		var errorMessage =
+			e.getCause().toString() + "\n" + e.getLocalizedMessage() + Arrays.toString(e.getStackTrace());
+
+		log.error("Exception -> {} | {}", e.getMessage(), errorMessage);
+		log.error("Request url {}", request.getRequestURL());
+
+		return ResponseEntity
+			.status(HttpStatus.BAD_REQUEST)
+			.body(
+				new ErrorDto(
+					e.getMessage(),
+					errorMessage
+				)
+			);
 	}
 }
--- src/main/java/kr/co/chingubang/error/dto/ErrorMessage.java
+++ src/main/java/kr/co/chingubang/error/dto/ErrorMessage.java
@@ -1,12 +1,25 @@
 package kr.co.chingubang.error.dto;
 
+import org.springframework.http.HttpStatus;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public enum ErrorMessage {
-	CONFLICT_ERROR("예기치 못한 에러가 발생했습니다.");
+	CONFLICT_ERROR(HttpStatus.BAD_REQUEST, "예기치 못한 에러가 발생했습니다."),
 
+	/**
+	 * User Error Message
+	 */
+	NO_EXISTS_USER_ERROR(HttpStatus.NOT_FOUND, "존재하지 않는 유저입니다."),
+
+	/**
+	 * Community Error Message
+	 */
+	NO_EXISTS_CATEGORY_ERROR(HttpStatus.NOT_FOUND, "존재하지 않는 카테고리입니다.");
+
+	private final HttpStatus status;
 	private final String description;
 }
